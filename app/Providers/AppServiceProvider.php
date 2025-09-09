93caa91220d5ab0fd42ebbb6f1985602f5f4df75
dimesangel <angelguillermo.velasquez@salud.gob.sv>
2025-09-09 13:05:58 -0600

    fix: arregla funcion de verificacion de correo electronico

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -97,7 +97,7 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         VerifyEmail::toMailUsing(function (object $notifiable, string $url){
-            $frontendUrl = 'http://localhost:8080/dashboard?url='. urlencode($url);
+            $frontendUrl = 'http://localhost:8080/verify-email?url='. urlencode($url);
             return (new MailMessage)
             ->subject('Verificar direccion de correo electronico')
             ->line('Por favor haz clic en el botón de abajo para verificar tu dirección de correo electrónico.')
--- src/modules/auth/infrastructure/controllers/AuthController.php
+++ src/modules/auth/infrastructure/controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace Src\modules\auth\infrastructure\controllers;
 use App\Http\Controllers\Controller;
 use App\Models\MntUser;
 use DateTimeImmutable;
+use Illuminate\Auth\Events\Verified;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Src\shared\infrastructure\HttpResponses;
@@ -83,17 +84,23 @@ class AuthController extends Controller
         ]);
 
         if (!Auth::attempt($credentials)) {
-            return response()->json(['message' => 'Unauthorized'], 401);
+            return $this->unauthorized("No autorizado");
+            //return response()->json(['message' => 'Unauthorized'], 401);
         }
 
         $user = MntUser::where('user_name', $request->user_name)->first();
+
+        if(!$user->hasVerifiedEmail()){
+            return $this->forbiden("Por favor verifica tu correo antes de iniciar sesión");
+        }
+
         $token = $user->createToken('authToken')->accessToken;
 
-        return response()->json([
+        return $this->success([
             'access_token' => $token,
             'token_type' => 'Bearer',
             'user' => $user
-        ]);
+        ], "Success");
     }
 
     public function verifyEmail(Request $request)
@@ -107,9 +114,22 @@ class AuthController extends Controller
         return $this->success([], "Correo de verificación enviado");
     }
 
-    public function receptionToValidate(EmailVerificationRequest $request)
+    public function receptionToValidate(Request $request, $id, $hash)
     {
-        $request->fulfill();
+        $user = MntUser::findOrFail($id);
+
+        if(!hash_equals(sha1($user->getEmailForVerification()), $hash)){
+            return $this->forbiden("Link invalido o expirado");
+        }
+
+        if($user->hasVerifiedEmail()){
+            return $this->success([], "Correo ya verificado");
+        }
+
+        //$request->fulfill();
+
+        $user->markEmailAsVerified();
+        event(new Verified($user));
 
         return $this->success([], "Correo verificado exitosamente");
     }
