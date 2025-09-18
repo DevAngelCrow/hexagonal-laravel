d9829a8508f2eec6b25bb8194fabdf95edf391a0
angel <angelguillermo.velasquez@salud.gob.sv>
2025-09-18 16:10:36 -0600

    fix: arregla response de la respuesta de rutas

--- src/modules/security/infrastructure/controllers/RouteController.php
+++ src/modules/security/infrastructure/controllers/RouteController.php
@@ -95,7 +95,7 @@ class RouteController extends Controller
     public function getAllRoutesWithParent(GetAllRouteRequest $request) {
         $routesCollection = $this->routeGetAllRoutesWithParent->run($request->query("page"), $request->query("per_page"));
         
-        $collections = array_map(fn($item) => RouteAggregateDtoHttp::fromEntity($item), $routesCollection["data"]);
+        $collections = array_map(fn($item) => RouteAggregateDtoHttp::fromEntity($item)->toArray(), $routesCollection["data"]);
         
         $paginateData = PaginatedResponseDto::fromPaginatedResponse($collections, $routesCollection["pagination"]);
         
