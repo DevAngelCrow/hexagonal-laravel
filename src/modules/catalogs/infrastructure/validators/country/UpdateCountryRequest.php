a315f2f52b3ea17eb0cecafca2b182d51325a94e
angel <angelguillermo.velasquez@salud.gob.sv>
2025-09-24 10:23:07 -0600

    fix: arregla el enpoint de la edición del registro de país

--- src/modules/catalogs/infrastructure/validators/country/UpdateCountryRequest.php
+++ src/modules/catalogs/infrastructure/validators/country/UpdateCountryRequest.php
@@ -11,14 +11,10 @@ class UpdateCountryRequest extends BaseRequest {
     public function rules() : array{
         return [
             "id" => "required|integer",
-            "street" => "required",
-            "street_number" => "required",
-            "neighborhood" => "required",
-            "id_district" => "required|integer",
-            "house_number" => "required",
-            "block" => "required",
-            "pathway" => "required",
-            "id_people" => "required|integer"
+            "name" => "required|string",
+            "abbreviation" => "required|string",
+            "code" => "required|string",
+            "active" => "boolean"
         ];
     }
 }
